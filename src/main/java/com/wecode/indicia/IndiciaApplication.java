24302b16d01a7230aa12b8d00040693c03c226c2
Ozo <ozoesono@yahoo.com>
2017-08-24 10:51:15 +0100

    First Commit - Base Springboot Project setup

---
+++ src/main/java/com/wecode/indicia/IndiciaApplication.java
@@ -0,0 +1,12 @@
+package com.wecode.indicia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndiciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndiciaApplication.class, args);
+	}
+}
